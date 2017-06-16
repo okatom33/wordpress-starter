cf149b083da6fc00dc56c3f4be499907430e1b74
umadash <yamato@casinodrive.local>
2017-06-16 18:20:16 +0900

    update

--- functions/remove.php
+++ functions/remove.php
@@ -5,16 +5,16 @@
 function remove_menus() {
     remove_menu_page('edit.php'); // 投稿
     remove_menu_page('edit-comments.php'); // コメント
-    remove_menu_page('tools.php'); // ツール
+    // remove_menu_page('tools.php'); // ツール
     //remove_menu_page('profile.php'); // プロファイル
     //remove_menu_page( 'index.php' );                  // ダッシュボード
     // remove_menu_page( 'upload.php' );                 // メディア
     // remove_menu_page( 'edit.php?post_type=page' );    // 固定ページ
     remove_menu_page( 'edit-comments.php' );          // コメント
-    remove_menu_page( 'themes.php' );                 // 外観
+    // remove_menu_page( 'themes.php' );                 // 外観
     // remove_menu_page( 'plugins.php' );                // プラグイン
     //remove_menu_page( 'users.php' );                  // ユーザー
-    remove_menu_page( 'options-general.php' );        // 設定
+    // remove_menu_page( 'options-general.php' );        // 設定
 }
 add_action('admin_menu', 'remove_menus');
 
