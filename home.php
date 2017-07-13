0dad94e4b405e4c3f587466d26988884c7050729
umadash <yamato@umadash.com>
2017-07-13 11:33:56 +0900

    update

--- home.php
+++ home.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
 
+<?php
+$args = array(
+    'numberposts' => 10,
+    'post_type' => 'product',
+    'order' => 'ASC'
+);
+$posts = get_posts( $args );
+var_dump($posts);
+?>
+<?php if ($posts): foreach($posts as $post): ?>
+
+<?php endforeach ?>
+<?php else: ?>
+    投稿が見つかりません
+<?php endif ?>
+
+
+
 <?php get_footer(); ?>
