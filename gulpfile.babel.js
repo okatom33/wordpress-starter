ecdf068bbb4d71768b20570e849ea17b107567b2
umadash <yamato@umadash.com>
2017-07-13 10:31:55 +0900

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,6 @@
 
 import gulp from 'gulp';
 import watch from 'gulp-watch';
-import pug from 'gulp-pug';
 import sass from 'gulp-sass';
 import sassGlob from 'gulp-sass-glob';
 import pleeease from 'gulp-pleeease';
@@ -15,35 +14,23 @@ import source from 'vinyl-source-stream';
 // 定数
 const SRC = './_src';
 const CONFIG = './_src/config';
-const PUBLIC = './public';
+const PUBLIC = './';
 const BASE_PATH = '';
 const DEST = `${PUBLIC}${BASE_PATH}`;
 
-// html
-gulp.task('pug', () => {
-  const locals = readConfig(`${CONFIG}/meta.yml`);
-  return gulp.src(`${SRC}/pug/**/[!_]*.pug`)
-    .pipe(pug({
-      locals: locals,
-      pretty: true,
-      basedir: `${SRC}/pug`
-    }))
-    .pipe(gulp.dest(`${DEST}`));
-});
-gulp.task('html', gulp.series('pug'));
-
-// css
+// css ////////////////////////////////////////////////
 gulp.task('sass', () => {
   const config = readConfig(`${CONFIG}/pleeease.json`);
   return gulp.src(`${SRC}/scss/style.scss`)
     .pipe(sassGlob())
     .pipe(sass())
     .pipe(pleeease(config))
-    .pipe(gulp.dest(`${DEST}/css`))
+    .pipe(gulp.dest(`${DEST}`))
 });
 gulp.task('css', gulp.series('sass'));
 
-// js
+
+// js /////////////////////////////////////////////////
 gulp.task('browserify', () => {
   return browserify(`${SRC}/js/app.js`)
     .transform(babelify)
@@ -53,17 +40,15 @@ gulp.task('browserify', () => {
 });
 gulp.task('js', gulp.series('browserify'));
 
+
 // browser-sync
 gulp.task('browser-sync', () => {
-  watch([
-    `${SRC}/pug/**/*.pug`,
-  ], gulp.series('pug', browserSync.reload));
-  watch([`${SRC}/scss/**/*.scss`], gulp.series('sass', browserSync.reload));
+  watch([`${SRC}/scss/**/*.scss`], gulp.series('css', browserSync.reload));
   watch([`${SRC}/js/**/*.js`], gulp.series('js', browserSync.reload));
 });
 gulp.task('serve', gulp.series('browser-sync'));
 
 
 // default
-gulp.task('build', gulp.parallel('html', 'css', 'js'));
+gulp.task('build', gulp.parallel('css', 'js'));
 gulp.task('default', gulp.parallel('build', 'serve'));
